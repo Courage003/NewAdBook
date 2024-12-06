27bf058e39180ae5c57b6e6fbff6a4e09b47ec6b
Courage003 <rastogidhruv28@gmail.com>
2024-12-06 18:08:01 +0530

    Added: Custom Linked List to add Data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        AddressBook<Contact> a1= new AddressBook<>();
+        System.out.println("Welcome to Addressbook");
+        Scanner sc = new Scanner(System.in);
+        int input=0;
+        do{
+            System.out.println("Enter 1 to add contact to Address Book\n Enter 2 to display all contacts");
+
+            int userInput = sc.nextInt();
+            switch (userInput){
+                case 1 -> a1.addToAdBook(Contact.createContact());
+                case 2 -> a1.display();
+                default -> System.out.println("Wrong Input");
+
+            }
+
+            System.out.println("Press 0 to exit");
+            input = sc.nextInt();
+        }while (input!=0);
         Contact c1=Contact.createContact();
-        System.out.println(c1);
+
 
 
 
